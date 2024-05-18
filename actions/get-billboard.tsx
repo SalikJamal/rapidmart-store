550b0914edf1a51f2be65b1bdacf2e0f5035abc8
Salik <iamsalikjamal@gmail.com>
2024-05-18 22:48:16 +0500

    Rendered billboards

--- actions/get-billboard.tsx
+++ actions/get-billboard.tsx
@@ -3,7 +3,7 @@ import { IBillboard } from "@/lib/types"
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/billboards`
 
-export const getBillboard = async (id: string): Promise<IBillboard[]> => {
+export const getBillboard = async (id: string): Promise<IBillboard> => {
     const res = await fetch(`${URL}/${id}`)
     return res.json()
 }
--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,14 +1,18 @@
+import { getBillboard } from "@/actions/get-billboard"
 import Billboard from "@/components/billboard"
 import Container from "@/components/ui/container"
 
 export const revalidate = 0
 
 
-export default function HomePage() {
+export default async function HomePage() {
+    
+    const billboard = await getBillboard("144e92e7-a956-4000-ba7f-ad1a38a1c98d")
+    
     return (
         <Container>
             <div className="space-y-10 pb-10">
-                <Billboard data={[]} />
+                <Billboard data={billboard} />
             </div>
         </Container>
     )
