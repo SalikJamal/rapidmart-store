6aa92fd0e7330c61bc115b2b8bac939504b3e41c
Salik <iamsalikjamal@gmail.com>
2024-05-18 22:44:25 +0500

    getBillboard server action

---
+++ actions/get-billboard.tsx
@@ -0,0 +1,9 @@
+import { IBillboard } from "@/lib/types"
+
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/billboards`
+
+export const getBillboard = async (id: string): Promise<IBillboard[]> => {
+    const res = await fetch(`${URL}/${id}`)
+    return res.json()
+}
