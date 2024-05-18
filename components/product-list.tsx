4a320718721c7708efcb9de414af9fff0a5014f1
Salik <iamsalikjamal@gmail.com>
2024-05-18 23:25:22 +0500

    Rendering products and no results component

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,4 +1,5 @@
 import { IProduct } from "@/lib/types"
+import NoResults from "@/components/ui/no-results"
 
 interface IProductListProps {
     items: IProduct[];
@@ -10,6 +11,14 @@ export default function ProductList({ items, title }: IProductListProps) {
     return (
         <div className="space-y-4">
             <h3 className="font-bold text-3xl">{title}</h3>
+            {!items.length && <NoResults />}
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                {items.map(item => (
+                    <div key={item.id}>
+                        {item.name}
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
---
+++ components/ui/no-results.tsx
@@ -0,0 +1,7 @@
+export default function NoResults() {
+    return (
+        <div className="flex justify-center items-center h-full w-full text-neutral-500">
+            No results found.
+        </div>
+    )
+}
