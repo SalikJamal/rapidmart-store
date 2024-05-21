84ace8a650bc230aa8874f57c06ddfa6b2a52906
Salik <iamsalikjamal@gmail.com>
2024-05-21 21:26:47 +0500

    getColors and getSizes action

---
+++ actions/get-colors.tsx
@@ -0,0 +1,9 @@
+import { IColor } from "@/lib/types"
+
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/colors`
+
+export const getColors = async (): Promise<IColor[]> => {
+    const res = await fetch(URL)
+    return res.json()
+}
---
+++ actions/get-sizes.tsx
@@ -0,0 +1,9 @@
+import { ISize } from "@/lib/types"
+
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/sizes`
+
+export const getSizes = async (): Promise<ISize[]> => {
+    const res = await fetch(URL)
+    return res.json()
+}
