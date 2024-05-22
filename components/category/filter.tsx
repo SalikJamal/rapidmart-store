3b4596a23fb31e5b369e668bb3cb3fab86eb3002
Salik <iamsalikjamal@gmail.com>
2024-05-22 16:11:48 +0500

    Filter component

--- components/category/filter.tsx
+++ components/category/filter.tsx
@@ -1,14 +1,65 @@
-import { ISize } from "@/lib/types"
+"use client"
+
+import qs from "query-string"
+import { useRouter, useSearchParams } from "next/navigation"
+import { IColor, ISize } from "@/lib/types"
+import Button from "@/components/ui/button"
+import { cn } from "@/lib/utils"
 
 interface IFilterProps {
     valueKey: string;
     name: string;
-    data: ISize[];
+    data: (ISize | IColor)[];
 }
 
 
 export default function Filter({ valueKey, name, data }: IFilterProps) {
+    
+    const searchParams = useSearchParams()
+    const router = useRouter()
+
+    const selectedValue = searchParams.get(valueKey)
+
+    const onClick = (id: string) => {
+        const current = qs.parse(searchParams.toString())
+
+        const query = {
+            ...current,
+            [valueKey]: id
+        }
+
+        if(current[valueKey] === id) {
+            query[valueKey] = null
+        }
+
+        const url = qs.stringifyUrl({
+            url: window.location.href,
+            query
+        }, { skipNull: true })
+
+        router.push(url)
+    }
+
     return (
-        <div>Filter</div>
+        <div className="mb-8">
+            <h3 className="text-lg font-semibold">
+                {name}
+            </h3>
+            <hr className="my-4" />
+            <div className="flex flex-wrap gap-2">
+                {data.map(filter => (
+                    <div className="flex items-center" key={filter.id}>
+                        <Button
+                            className={cn(`rounded-md text-sm text-gray-800
+                            bg-white border border-gray-300 p-2`, 
+                            selectedValue === filter.id && `bg-black text-white`)}
+                            onClick={() => onClick(filter.id)}
+                        >
+                            {filter.name}
+                        </Button>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
